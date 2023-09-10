8b9fea27f019fc0f1f8622fde46efe92a2c14a46
haxy <zaidsaheb4@gmail.com>
2023-09-10 02:45:23 +0200

    did exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,11 +3,24 @@
   <h1>{props.course}</h1> )
  }
 
-const Content = (props) => {
+
+
+ const Part = (props) => {
   return (
        <p>
         {props.part} {props.exercises}
       </p>
+  )
+}
+
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
   )
 }
 
@@ -35,9 +48,12 @@ const App = () => {
       <Header course={course} />
 
       <div>
-        <Content part = {part1} exercises = {exercises1} />
-        <Content part = {part2} exercises = {exercises2} />
-        <Content part = {part3} exercises = {exercises3} />
+        <Content 
+        part1= {part1} exercises1 = {exercises1} 
+        part2= {part2} exercises2 = {exercises2} 
+        part3= {part3} exercises3 = {exercises3} 
+        />
+
       </div>
 
       <Total sum1={exercises1} sum2={exercises2} sum3={exercises3} />
